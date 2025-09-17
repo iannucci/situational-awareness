b25e10ae89ac55156398faba37018644c4d57112
Bob Iannucci <bob@rail.com>
2025-09-17 16:09:53 -0700

    Debugging

--- install.sh
+++ install.sh
@@ -496,10 +496,9 @@ rm -f /tmp/situational-awareness-nginx.conf
 echo -e "${BLUE}Creating meshtastic-client.sh...${NC}"
 
 cat > "$APP_DIR/src/info-sources/meshtastic-client.sh" << MESHTASTICCLIENTSH
-#!/bin/bash
+#! /bin/bash
 
-cd /root/meshtastic-client
-source base/bin/activate
+source /root/meshtastic-client/base/bin/activate
 python3 $APP_DIR/src/info-sources/meshtastic-client.py --config $ETC_DIR/config.json
 MESHTASTICCLIENTSH
 
