4d375c0c6b841e75ca26646baf332e145434ff61
Bob Iannucci <bob@rail.com>
2025-09-25 10:11:22 -0700

    Debugging

--- src/api/routes/logs.js
+++ src/api/routes/logs.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 router.post('/entry', async (req, res) => {
 	console.log(req.body);
+	level = req.body.level
 	message = req.body.message;
-	console.log("<CLIENT> ", message);
+	console.log("<CLIENT> ", level.toUpperCase(), message);
 	return res.json({
 		success: true,
 		data: [],
